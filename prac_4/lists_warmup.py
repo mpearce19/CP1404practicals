d5913636978f142e8cef6b5549ff44bbed143eb1
mpearce19 <mitchell.pearce@my.jcu.edu.au>
2020-08-28 09:40:30 +1000

    finished warmup

---
+++ prac_4/lists_warmup.py
@@ -0,0 +1,10 @@
+# 1. List Warm up
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 3, 2, 1, (3, 1, 4, 1, 5, 9), 1, true, false, false, add the numbers
+
+numbers.insert(0, "ten")
+numbers.insert(-1, 1)
+numbers[2:]
+9 in numbers
