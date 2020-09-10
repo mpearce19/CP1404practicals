253258856134294391a2e76036dc9560a3f24f8e
mpearce19 <mitchell.pearce@my.jcu.edu.au>
2020-09-10 21:00:03 +1000

    initial commit

---
+++ prac_6/languages.py
@@ -0,0 +1,18 @@
+from prac_6.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+    languages = [ruby, python, visual_basic]
+
+    print("The dynamically types languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
