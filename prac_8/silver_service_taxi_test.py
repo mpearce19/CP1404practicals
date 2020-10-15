7163111ea2fb81658d6ccbdf42a57eb2fb40f211
mpearce19 <mitchell.pearce@my.jcu.edu.au>
2020-10-15 16:48:22 +1000

    complete and test silver service taxi

---
+++ prac_8/silver_service_taxi_test.py
@@ -0,0 +1,13 @@
+"""silver service taxi tests"""
+
+from prac_8.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+
+main()
